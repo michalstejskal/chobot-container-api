fed8a8f4ea9d8a0d43a39b6b01d26a3216f48dd6
Michal Stejskal <mstejskal@newps.cz>
2018-10-18 22:57:25 +0200

    find user methods

--- src/main/kotlin/cz/chobot/container_api/service/IUserService.kt
+++ src/main/kotlin/cz/chobot/container_api/service/IUserService.kt
@@ -1,8 +1,10 @@
 package cz.chobot.container_api.service
 
 import cz.chobot.container_api.bo.User
+import java.util.*
 
 interface IUserService{
+    fun findUser(idUser: Long): Optional<User>
     fun createUser(user: User): User
     fun updateUser(user: User): User
 }
--- src/main/kotlin/cz/chobot/container_api/service/impl/UserService.kt
+++ src/main/kotlin/cz/chobot/container_api/service/impl/UserService.kt
@@ -4,15 +4,25 @@ import cz.chobot.container_api.bo.User
 import cz.chobot.container_api.exception.ControllerException
 import cz.chobot.container_api.repository.UserRepository
 import cz.chobot.container_api.service.IUserService
+import org.apache.catalina.mbeans.UserMBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class UserService : IUserService {
 
-
     @Autowired
     private lateinit var userRepository: UserRepository
+
+    override fun findUser(idUser: Long): Optional<User> {
+        val userOpt = userRepository.findById(idUser)
+        if(userOpt.isPresent){
+            val user = userOpt.get()
+            return Optional.of(user)
+        }
+        return userOpt
+    }
 
     override fun createUser(user: User): User {
         user.login = user.login.toLowerCase()
